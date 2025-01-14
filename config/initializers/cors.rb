0363b8ba30c4e2dd9ac9449b3c4cc54e7ce491e5
troccoli <pedroaugustobelzkai@gmail.com>
2025-01-14 20:09:10 -0300

    fix: add the right cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:5173'
+    origins 'https://app.victusjournal.com', 'https://dev.victusjournal.com', 'http://localhost:5275'
 
     resource '*',
       headers: :any,

971ec1f4db959916d8447c8c89e6590b76993986
troccoli <pedroaugustobelzkai@gmail.com>
2025-01-18 23:39:25 -0300

    feat: add recurrency fields to habit controller and model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,11 +2,6 @@
     include Mongoid::Document
     include Mongoid::Timestamps
 
-    validates :name, presence: true
-    validates :start_date, presence: true
-    validates :end_date, presence: true
-    validate :date_validation
-
     belongs_to :account
     has_many :habit_checks
 
@@ -18,10 +13,25 @@
 
     field :last_check, type: Date
 
+    # infinite, daily, weekly, monthly, yearly
     field :recurrence_type, type: String
     field :recurrence_details, type: Hash
 
+    # Validations
+    validates :name, presence: true
+    validates :start_date, presence: true
+    validates :recurrence_type, presence: true
+
+    validate :date_validation
+    validate :recurrence_type_validation
+
     private
+
+    def recurrence_type_validation
+      if recurrence_type.present? && !%w(infinite daily weekly monthly yearly).include?(recurrence_type)
+        errors.add(:recurrence_type, "must be one of: infinite, daily, weekly, monthly, yearly")
+      end
+    end
 
     def date_validation
       start_date_obj = Date.parse(start_date) if start_date.present?

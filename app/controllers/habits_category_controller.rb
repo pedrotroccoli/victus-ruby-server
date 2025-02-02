5b1834d5d820ff73855f59df0570129aded54804
troccoli <pedroaugustobelzkai@gmail.com>
2025-02-02 16:34:42 -0300

    feat: create HabitCategoryController

---
+++ app/controllers/habits_category_controller.rb
@@ -0,0 +1,36 @@
+class HabitsCategoryController < ApplicationController
+  before_action :authorize_request
+
+  def index
+    @habits_categories = HabitCategory.where(account_id: @current_account[:id]).order(order: :asc)
+
+    render json: @habits_categories
+  end
+
+  def create
+    @habits_category = HabitCategory.new(habits_category_params)
+    @habits_category.account_id = @current_account[:id]
+
+    @habits_category.save!
+
+    render json: @habits_category, status: :created
+  end
+
+  def update
+    @habits_category = HabitCategory.find(params[:id])
+    @habits_category.update(habits_category_params)
+
+    render json: @habits_category, status: :ok
+  end
+
+  def destroy
+    @habits_category = HabitCategory.find(params[:id])
+    @habits_category.destroy
+  end
+
+  private
+
+  def habits_category_params
+    params.require(:habits_category).permit(:name, :order)
+  end
+end

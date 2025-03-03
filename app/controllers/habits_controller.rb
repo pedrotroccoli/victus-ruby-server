d6bca287a6f24756b3125bb19bb076c8bef54c5b
troccoli <pedroaugustobelzkai@gmail.com>
2025-03-03 10:55:21 -0300

    feat: add destroy action to HabitsController

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,9 +1,8 @@
 class HabitsController < ApplicationController
   before_action :authorize_request
+  before_action :set_habit, only: [:show, :update, :destroy]
 
   def show
-    @habit = Habit.where(account_id: @current_account[:id]).find(params[:id])
-
     render json: @habit
   end
 
@@ -27,8 +26,6 @@ class HabitsController < ApplicationController
   end
 
   def update
-    @habit = Habit.where(account_id: @current_account[:id]).find(params[:id])
-
     @habit.update(update_params)
 
     render json: @habit, include: :habit_category, status: :ok
@@ -44,7 +41,20 @@ class HabitsController < ApplicationController
   rescue => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
+
+  def destroy
+    @habit.destroy
+
+    render json: { message: 'Hábito deletado com sucesso' }, status: :ok
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
   private
+
+  def set_habit
+    @habit = Habit.where(account_id: @current_account[:id]).find(params[:id])
+  end
 
   def habit_params
     params.require(:habit).permit(:name, :description, :start_date, :end_date, :recurrence_type, recurrence_details: [:rule])

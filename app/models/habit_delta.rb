96085f08b3e57836929378e5a59828c38d871824
troccoli <pedroaugustobelzkai@gmail.com>
2025-03-04 17:26:48 -0300

    feat: add HabitDelta model and delta configuration to Habit

---
+++ app/models/habit_delta.rb
@@ -0,0 +1,28 @@
+class HabitDelta
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Paranoia
+
+  field :type, type: String
+  field :name, type: String
+  field :description, type: String
+  field :enabled, type: Boolean, default: true
+
+  validates :type, presence: true
+  validates :name, presence: true
+
+  embedded_in :habit
+  
+  def self.types
+    %w(number string)
+  end
+
+  validate :validate_type
+
+  private
+    def validate_type
+      unless self.class.types.include?(type)
+        errors.add(:type, "must be one of #{self.class.types.join(', ')}")
+      end
+    end
+end

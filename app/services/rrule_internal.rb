0a180cae9782032b9f5a8c31133c007bac0b51cd
troccoli <pedroaugustobelzkai@gmail.com>
2025-03-04 17:26:22 -0300

    feat: enhance RruleInternal with range validation and improved RRULE parsing

--- app/services/rrule_internal.rb
+++ app/services/rrule_internal.rb
@@ -1,8 +1,15 @@
-RRULE_REGEX = /\AFREQ=(DAILY|WEEKLY|MONTHLY|YEARLY)(;UNTIL=\d{8}T\d{6}Z)?(;[A-Z]+=[^;]+)*\z/
+# FREQ=DAILY;INTERVAL=1;UNTIL=20250327T000000Z
+RRULE_REGEX = /\AFREQ=(?<freq>DAILY|WEEKLY|MONTHLY|YEARLY)(?:;UNTIL=(?<until>\d{8}T\d{6}Z))?(?:;INTERVAL=(?<interval>\d+))?(?:;BYDAY=(?<byday>[^;]+))?(?:;[A-Z]+=?[^;]+)*\z/
 
 class RruleInternal
+  def initialize(rrule)
+    self.class.validate_rrule!(rrule)
+    
+    @rrule = rrule
+  end
+
   def self.validate_rrule(rrule)
-    # FREQ=DAILY;INTERVAL=1;UNTIL=20250327T000000Z
+    rrule ||= @rrule
 
     if rrule.match(RRULE_REGEX)
       return true
@@ -12,8 +19,46 @@ class RruleInternal
   end
 
   def self.validate_rrule!(rrule)
+    rrule ||= @rrule
+
     if !validate_rrule(rrule)
       raise RRule::InvalidRRuleError, "Invalid RRULE"
+    end
+  end
+
+  def is_in_range?(date)
+    parse_date = DateTime.parse(date.to_s)
+
+    if parse_date.nil?
+      raise RRule::InvalidRRuleError, "Invalid date"
+    end
+    match_data = @rrule.match(RRULE_REGEX)
+
+    until_date = match_data[:until]
+    permitted_days = match_data[:byday]
+
+    if until_date.present?
+      until_date = DateTime.parse(until_date)
+
+      if parse_date > until_date
+        return false
+      end
+    end
+
+    if permitted_days.present?
+      permitted_days = permitted_days.split(",")
+      
+      if !permitted_days.include?(parse_date.strftime("%A").upcase[0, 2])
+        return false
+      end
+    end
+
+    return true
+  end
+
+  def is_in_range!(date)
+    if !is_in_range?(date)
+      raise RRule::InvalidRRuleError, "Date is not in range"
     end
   end
 end

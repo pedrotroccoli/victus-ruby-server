d4b4acd1852051e7c3176a54a723696c8ff5a9ac
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-01 17:22:58 -0300

    feat: restructure routes to include private, public, and internal modules for better organization and clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,43 @@
 Rails.application.routes.draw do
-  get 'ping', to: 'ping#index'
-  post 'ping', to: 'ping#index'
+    get 'ping', to: 'ping#index'
+    post 'ping', to: 'ping#index'
 
-  scope :api do
-    scope :v1 do
-      get 'auth/test', to: 'auth#test'
+    scope :api do
+      scope :v1 do
+        scope module: 'private' do
+          get 'auth/test', to: 'auth#test'
 
-      post 'auth/sign-in', to: 'auth#sign_in'
-      post 'auth/sign-up', to: 'auth#sign_up'
-      get 'auth/me', to: 'auth#me'
+          get 'me', to: 'me#me'
 
-      get 'habits/:id', to: 'habits#show'
-      get 'habits', to: 'habits#index'
-      post 'habits', to: 'habits#create'
-      put 'habits/:id', to: 'habits#update'
-      delete 'habits/:id', to: 'habits#destroy'
+          get 'habits/:id', to: 'habits#show'
+          get 'habits', to: 'habits#index'
+          post 'habits', to: 'habits#create'
+          put 'habits/:id', to: 'habits#update'
+          delete 'habits/:id', to: 'habits#destroy'
 
-      get 'habits-check', to: 'habits_check#all'
-      get 'habits-check/:habit_id', to: 'habits_check#index'
-      get 'habits-check/:habit_id/:check_id', to: 'habits_check#show'
-      post 'habits-check/:habit_id', to: 'habits_check#create'
-      put 'habits-check/:habit_id/:check_id', to: 'habits_check#update_check'
+          get 'habits-check', to: 'habits_check#all'
+          get 'habits-check/:habit_id', to: 'habits_check#index'
+          get 'habits-check/:habit_id/:check_id', to: 'habits_check#show'
+          post 'habits-check/:habit_id', to: 'habits_check#create'
+          put 'habits-check/:habit_id/:check_id', to: 'habits_check#update_check'
 
-      resources :habits_category, only: [:index, :create, :update, :destroy]
+          resources :habits_category, only: [:index, :create, :update, :destroy]
+          resources :mood
+
+          post 'checkout/create', to: 'checkout#create'
+
+          get 'plans', to: 'plans#index'
+      end
+
+      scope module: 'public' do
+
+        post 'auth/sign-in', to: 'auth#sign_in'
+        post 'auth/sign-up', to: 'auth#sign_up'
+      end
+
+      scope module: 'internal' do
+        post 'stripe/webhook', to: 'stripe_webhook#webhook'
+      end
     end
   end
 end

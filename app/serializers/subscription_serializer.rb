9336c962f8c31e8d9d1c7f4b8f6241f6ce72b91e
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-01 22:47:23 -0300

    feat: add sub_status attribute to SubscriptionSerializer for enhanced subscription details

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,5 +1,5 @@
 class SubscriptionSerializer < ActiveModel::Serializer
-  attributes :id, :status, :service_type, :service_details
+  attributes :id, :status, :service_type, :service_details, :sub_status
 
   has_one :account, serializer: AccountSerializer
 end

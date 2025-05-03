1b21a610e8d9a609a7f4451b8a9e6ceb41d45f89
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-03 18:00:17 -0300

    feat: implement SIWE authentication flow with nonce handling and AWS Lambda verification; update account connection logic for web and worldapp providers

--- app/controllers/public/auth_controller.rb
+++ app/controllers/public/auth_controller.rb
@@ -6,6 +6,11 @@ module Public
       account = Account.find_by(email: accounts_params[:email])
 
       if account && account.authenticate(accounts_params[:password])
+        if !account.connected_providers.include?('web')
+          account.connected_providers << 'web'
+          account.save
+        end
+
         token = JWT.encode({ account_id: account.id }, ENV['JWT_SECRET'], 'HS256')
 
         render json: { message: 'Signed in successfully', token: token }, status: :ok
@@ -83,6 +88,105 @@ module Public
       else
         render json: { message: 'Something went wrong' }, status: :unauthorized
       end
+    end
+
+    def start_siwe_auth
+      #nonce = Siwe::Util.generate_nonce
+      nonce = '451999bb'
+
+      cookies.signed[:siwe_nonce] = {
+        value: nonce,
+        path: '/',
+        httponly: true,
+        secure: Rails.env.production?
+      }
+
+      puts "\n\n #{cookies.signed[:siwe_nonce]} \n\n"
+
+      render json: { nonce: nonce }, status: :ok
+    end
+
+    def siwe_verify
+      siwe_params = params[:payload]
+      nonce = params[:nonce]
+
+      if siwe_params.blank? || nonce.blank?
+        return render json: { message: 'Invalid message -1' }, status: :unauthorized
+      end
+
+      # Initialize AWS Lambda client
+      lambda_client = Aws::Lambda::Client.new(
+        region: 'us-east-1',
+      )
+
+      lambda_payload = {
+        payload: siwe_params,
+        nonce: nonce
+      }.to_json
+
+      lambda_response = lambda_client.invoke({
+        function_name: 'victus-siwe-dev-world-siwe-verify',
+        invocation_type: 'RequestResponse',
+        payload: lambda_payload
+      })
+
+      response_body = JSON.parse(lambda_response.payload.read)
+
+      if lambda_response.status_code != 200
+        return render json: { message: 'Something went wrong' }, status: :unauthorized
+      end
+
+      if response_body['valid'] == false
+        return render json: { message: 'Invalid message' }, status: :unauthorized
+      end
+
+      account = Account.find_or_create_by(world_address: response_body['data']['address'])
+
+      if !account.new_record?
+        token = JWT.encode({ account_id: account.id }, ENV['JWT_SECRET'], 'HS256')
+
+        if !account.connected_providers.include?('worldapp')
+          account.connected_providers << 'worldapp'
+          account.save
+        end
+
+        if account.subscription.nil?
+          account.subscription = Subscription.new(
+          status: 'active',
+          sub_status: 'active',
+          service_type: 'worldapp',
+          service_details: {
+            # trial_ends_at: 14.days.from_now
+          }
+          )
+
+        account.subscription.save
+
+        end
+
+        return render json: { message: 'Signed in successfully', token: token, account: account }, status: :ok
+      end
+
+      account.connected_providers << 'worldapp'
+
+      account.subscription = Subscription.new(
+        status: 'active',
+        sub_status: 'active',
+        service_type: 'worldapp',
+        service_details: {
+          # trial_ends_at: 14.days.from_now
+        }
+      )
+      if account.save
+        token = JWT.encode({ account_id: account.id }, ENV['JWT_SECRET'], 'HS256')
+
+        render json: { message: 'Signed in successfully', token: token }, status: :ok
+      else
+        render json: { message: 'Something went wrong' }, status: :unauthorized
+      end
+      
+    rescue Exception => e
+      render json: { message: 'Invalid message 1', error: e }, status: :unauthorized
     end
   end
 end

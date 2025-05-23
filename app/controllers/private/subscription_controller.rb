2efb8ec3f086d27de378db3830df0ead567d9e94
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-23 14:49:53 -0300

    feat: implement SubscriptionController with create_session action for Stripe integration

---
+++ app/controllers/private/subscription_controller.rb
@@ -0,0 +1,12 @@
+module Private
+  class SubscriptionController < Private::PrivateController
+    def create_session
+      stripe_session = ::StripeService.new.create_session({
+        customer: @current_account.subscription.service_details['customer_id'].to_s,
+        return_url: "#{ENV['APP_URL']}/account/subscription"
+      })
+
+      render json: { session_url: stripe_session.url }, status: :ok
+    end
+  end
+end

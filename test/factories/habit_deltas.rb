baf4aaed810b7240b3c8f5eed1ceb300b973eea7
troccoli <pedroaugustobelzkai@gmail.com>
2025-12-06 22:53:44 -0300

    feat: add HabitDelta factory for testing with various types and traits

---
+++ test/factories/habit_deltas.rb
@@ -0,0 +1,27 @@
+FactoryBot.define do
+  factory :habit_delta do
+    association :habit
+    type { "number" }
+    name { "Duration" }
+    description { "Exercise duration in minutes" }
+    enabled { true }
+    version { "v2" }
+
+    trait :string_type do
+      type { "string" }
+      name { "Notes" }
+      description { "Additional notes" }
+    end
+
+    trait :time_type do
+      type { "time" }
+      name { "Start Time" }
+      description { "When to start" }
+    end
+
+    trait :disabled do
+      enabled { false }
+    end
+  end
+end
+

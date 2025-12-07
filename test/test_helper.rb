e897399a499be1e24d1175a9f03dc3253ea10555
troccoli <pedroaugustobelzkai@gmail.com>
2025-12-06 22:55:34 -0300

    feat: enhance test helpers with FactoryBot integration and add authentication header method for integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,25 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all  # Disabled: Using MongoDB (Mongoid) instead of ActiveRecord
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  # Helper method to generate authentication headers for a given account
+  def auth_headers(account)
+    token = JWT.encode({ account_id: account.id.to_s }, ENV['JWT_SECRET'], 'HS256')
+    {
+      'Authorization' => "Bearer #{token}",
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+  end
 end

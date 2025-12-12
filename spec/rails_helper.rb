2e66d36797275980ccd12d619a2781ecb82bd0fd
troccoli <pedroaugustobelzkai@gmail.com>
2025-12-12 17:22:46 -0300

    chore: add RSpec configuration files for testing setup and integration with FactoryBot

---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+# This file loads Rails environment for RSpec tests
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+
+  # Include FactoryBot methods
+  config.include FactoryBot::Syntax::Methods
+
+  # Clean database between tests (Mongoid doesn't use transactions)
+  config.before(:each) do
+    Mongoid.purge!
+  end
+end
+

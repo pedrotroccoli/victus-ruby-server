dbd189c91afd5b9e75213655989885eb0c1194a1
troccoli <pedroaugustobelzkai@gmail.com>
2025-12-13 11:21:44 -0300

    feat: enhance habit creation process by adding account validation and associating account with habit

--- app/operations/habits/create.rb
+++ app/operations/habits/create.rb
@@ -1,3 +1,5 @@
+require_relative '../../contracts/habits/create_habit'
+
 module Habits
   class Create < Trailblazer::Operation
     step :validate_params
@@ -6,17 +8,20 @@ module Habits
     step :build_habit_deltas
     step :save_habit
 
-    def validate_params(ctx, params:, **)
+    def validate_params(ctx, params:, account:, **)
       ctx[:errors] = []
 
-      unless params[:account_id].present?
-        ctx[:errors] << "Account ID is required"
+      unless account.present?
+        ctx[:errors] << "Account is required"
+        return false
       end
 
-      CreateHabitContract.new.call(params)
+      params_with_account = params.merge(account_id: account.id.to_s)
 
-      if result.failure?
-        ctx[:errors] << result.errors.full_messages
+      contract_result = CreateHabitContract.new.call(params_with_account)
+
+      if contract_result.failure?
+        ctx[:errors] << contract_result.errors
       end
 
       ctx[:errors].empty?
@@ -26,6 +31,11 @@ module Habits
       habit_params = params.except(:habit_deltas)
 
       ctx[:habit] = Habit.new(habit_params)
+    end
+
+    def assign_account(ctx, account:, **)
+      ctx[:habit].account = account
+      true
     end
 
     def build_habit_deltas(ctx, params:, **)

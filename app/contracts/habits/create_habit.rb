8140302d729107d5d79d9ca8422821d400e0fe1a
troccoli <pedroaugustobelzkai@gmail.com>
2025-12-13 00:16:58 -0300

    fix: enhance recurrence validation in CreateHabitContract to enforce rule presence and date constraints

--- app/contracts/habits/create_habit.rb
+++ app/contracts/habits/create_habit.rb
@@ -5,7 +5,7 @@ class CreateHabitContract < Dry::Validation::Contract
     required(:start_date).filled(:date)
     optional(:end_date).filled(:date)
 
-    required(:recurrence_type).filled(:string)
+    required(:recurrence_type).filled(:symbol, included_in?: [:infinite, :daily, :weekly, :monthly, :yearly])
     required(:recurrence_details).filled(:hash)
 
     required(:rule_engine_enabled).filled(:bool)
@@ -44,6 +44,36 @@ class CreateHabitContract < Dry::Validation::Contract
     unless logic[type.to_sym].all? { |condition| condition.is_a?(String) }
       key.failure("#{type} condition must be an array of strings")
       next
+    end
+  end
+  
+  rule(:recurrence_details) do
+    if value.blank? || value[:rule].blank?
+      key.failure('rule is required')
+      next
+    end
+
+    if RruleInternal.validate_rrule(value[:rule])
+      key.failure('rule is invalid')
+      next
+    end
+  end
+
+  rule(:start_date, :end_date) do
+    start_date = values[:start_date]
+
+    if start_date < Date.today
+      key(:start_date).failure('must be today or in the future')
+      next
+    end
+
+    end_date = values[:end_date]
+
+    next if end_date.blank?
+
+    if start_date > end_date
+      key(:start_date).failure('must be before end_date')
+      key(:end_date).failure('must be after start_date')
     end
   end
 end

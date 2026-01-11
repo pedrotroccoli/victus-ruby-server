085e1849c0721bf9474c8f0c8f3d5b1cf33dab52
troccoli <pedroaugustobelzkai@gmail.com>
2026-01-10 23:50:33 -0300

    feat: create mood controller with basic CRUD operations

--- app/controllers/private/mood_controller.rb
+++ app/controllers/private/mood_controller.rb
@@ -1,5 +1,53 @@
-class Private::MoodController < Private::PrivateController
-  def index
-    render json: { message: 'Mood area' }, status: :ok
+module Private
+  class MoodController < Private::PrivateController
+    before_action :set_mood, only: [:show, :update, :destroy]
+
+    def index
+      @moods = Mood.where(account_id: @current_account[:id])
+        .order_by(created_at: :desc)
+
+      render json: @moods, status: :ok
+    end
+
+    def show
+      render json: @mood, status: :ok
+    end
+
+    def create
+      @mood = Mood.new(mood_params)
+      @mood.account = @current_account
+
+      if @mood.save
+        render json: @mood, status: :created
+      else
+        render json: { errors: @mood.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @mood.update(mood_params)
+        render json: @mood, status: :ok
+      else
+        render json: { errors: @mood.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @mood.destroy
+
+      render json: { message: 'Humor deletado com sucesso' }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+
+    private
+
+    def set_mood
+      @mood = Mood.where(account_id: @current_account[:id]).find(params[:id])
+    end
+
+    def mood_params
+      params.require(:mood).permit(:value, :description)
+    end
   end
 end

ea0a0f69aa2c080eafc7a0150d688729cf8c8c15
troccoli <pedroaugustobelzkai@gmail.com>
2026-01-22 11:45:32 -0300

    refactor: refactor stripe service

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,60 +1,39 @@
-require 'stripe'
-
 class StripeService
-  def create_checkout(params)
-    account_id = params[:account_id]
-    lookup_key = params[:lookup_key]
-    customer_id = params[:customer_id]
-
-    price = Stripe::Price.list(
-      lookup_keys: [lookup_key],
-      expand: ['data.product']
+  def create_customer(email:, name: nil, metadata: {})
+    Stripe::Customer.create(
+      email: email,
+      name: name,
+      metadata: metadata
     )
+  end
 
-    stripe::Checkout::Session.create(
+  def create_billing_portal_session(customer_id:, return_url:)
+    Stripe::BillingPortal::Session.create(
       customer: customer_id,
-      mode: 'subscription',
-      line_items: [{ price: price.data.first.id, quantity: 1 }],
-      success_url: "#{ENV['APP_URL']}/checkout/?checkout_success=true",
-      cancel_url: "#{ENV['APP_URL']}/checkout/?checkout_cancel=true",
-      metadata: {
-        account_id: account_id,
-        lookup_key: lookup_key
-      },
-      subscription_data: {
-        trial_period_days: 14
-      },
-      allow_promotion_codes: true
+      return_url: return_url
     )
-  end
-
-  def create_checkout_session(params)
-    stripe::Checkout::Session.create({
-      customer: params[:customer],
-      mode: 'subscription',
-      line_items: [{ price: params[:price] }]
-    })
-  end
-
-  def create_customer(account_information)
-    email = account_information[:email]
-
-    stripe::Customer.create(email: email)
   end
 
   def create_subscription_session(params)
-    stripe::BillingPortal::Session.create({
+    Stripe::BillingPortal::Session.create(
       customer: params[:customer],
       return_url: params[:return_url]
-    })
+    )
   end
 
-  private
-
-  def stripe
-    @stripe ||= begin
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-      Stripe
+  def cancel_subscription(subscription_id, cancel_at_period_end: true)
+    if cancel_at_period_end
+      Stripe::Subscription.update(subscription_id, { cancel_at_period_end: true })
+    else
+      Stripe::Subscription.cancel(subscription_id)
     end
   end
-end 
+
+  def get_subscription(subscription_id)
+    Stripe::Subscription.retrieve(subscription_id)
+  end
+
+  def list_invoices(customer_id:, limit: 10)
+    Stripe::Invoice.list(customer: customer_id, limit: limit)
+  end
+end

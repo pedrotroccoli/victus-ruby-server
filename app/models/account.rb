0c5e6ed6f45ff7a2f8ea4e9b30c8e1ebef891f4a
troccoli <pedroaugustobelzkai@gmail.com>
2026-01-22 09:48:48 -0300

    feat: add google integration to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -13,9 +13,19 @@ class Account
   field :password_digest, type: String
   field :phone, type: String, default: nil
   field :world_address, type: String, default: nil
+  field :google_id, type: String, default: nil
   field :connected_providers, type: Array, default: ['web']
 
   validates :world_address, uniqueness: true, allow_nil: true
+  validates :google_id, uniqueness: true, allow_nil: true
 
   has_secure_password validations: false
+
+  def create_trial_subscription
+    self.subscription = Subscription.new(
+      status: 'pending',
+      sub_status: 'pending_payment_information',
+      service_details: { trial_ends_at: 14.days.from_now }
+    )
+  end
 end
